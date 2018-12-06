c636113d0e1a29ef41d4061db65b24ae52403f6b
Jailson <jailson.cavalcanti@biqhs.com>
2018-12-06 11:22:00 +0000

    Add css for darken div

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 <div class="container">
 	
 	<div id="container-center" class="container-center">
-		
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mi sapien, venenatis at bibendum quis, rutrum a sapien. Suspendisse nulla felis, eleifend id magna tempor, euismod imperdiet sapien. In in ligula dictum, vestibulum libero convallis, pulvinar nunc. Mauris lacinia eros eget suscipit cursus. Ut eget commodo neque. Quisque tempus orci sed mollis rhoncus. Quisque lacinia enim nibh, sit amet mattis dolor pretium sed. Maecenas eget suscipit nunc. Vestibulum eros elit, dictum tempus malesuada eget, luctus eget odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec posuere tincidunt diam, vel hendrerit odio. In ornare, odio eu commodo imperdiet, nisl quam luctus ipsum, vitae maximus ligula libero in augue. Quisque imperdiet, dui eu viverra faucibus, erat magna porta massa, at aliquet urna tortor sed tellus. Nam dapibus massa risus. Aliquam sem dui, ullamcorper quis congue maximus, sodales nec elit. Ut aliquet velit at leo molestie, ut tincidunt leo placerat.
+		</p>
 		<div class="button" id="button-center">Click</div>
 
 	</div>
