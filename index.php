934eab2dc683f81e6cd5cc6947153ae308764992
Jailson Cavalcanti <jailsoncavalcanti@Jailsons-MacBook-Air.local>
2018-12-06 08:29:49 +0000

    First Commit
    First Commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste Event</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+
+<div class="container">
+	
+	<div class="container-top"></div>
+	<div class="container-right"></div>
+	<div class="container-botton"></div>
+	<div class="container-left"></div>
+	<div class="container-center"></div>
+
+</div>
+
+
+<script src="js/jquery-3.3.1.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
